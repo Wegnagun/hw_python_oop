a82eaed667a23574e357386d6ccd2f180bbc806c
Александр Фокин <wegnagun@bk.ru>
2022-03-11 21:15:39 +0300

    date: 11.03.2022, more mistakes had changed

--- homework.py
+++ homework.py
@@ -1,16 +1,10 @@
 from dataclasses import dataclass
-from typing import List
-import abc
+from typing import List, Dict, Type
 
 
 @dataclass
 class InfoMessage:
     """Информационное сообщение о тренировке."""
-    training_type: str
-    duration: float
-    distance: float
-    speed: float
-    calories: float
     training_type: str
     duration: float
     distance: float
@@ -27,9 +21,9 @@ class InfoMessage:
 
 class Training:
     """Базовый класс тренировки."""
-    LEN_STEP: float = 0.65  # переменная для перевода движений в шаг, бег
+    LEN_STEP: float = 0.65  # константа для перевода движений в шаг, бег
     M_IN_KM: int = 1000  # константа для перевода м в км
-    HOUR_IN_MIN: int = 60  # константа для перевода часов в минуты
+    HOUR_TO_MIN: int = 60  # константа для перевода часов в минуты
 
     def __init__(self,
                  action: int,
@@ -48,10 +42,11 @@ class Training:
         """Получить среднюю скорость движения."""
         return self.get_distance() / self.duration
 
-    @abc.abstractmethod
     def get_spent_calories(self) -> float:
         """Получить количество затраченных калорий."""
-        pass  # TODO: переопределить в других классах
+        raise ChildProcessError(f'Класс {self.__class__.__name__} '
+                                f'должен реализовывать метод '
+                                f'get_spent_calories')
 
     def show_training_info(self) -> InfoMessage:
         """Вернуть информационное сообщение о выполненной тренировке."""
@@ -64,22 +59,22 @@ class Training:
 
 class Running(Training):
     """Тренировка: бег."""
-    frst_run_multiplier: int = 18
-    scnd_run_multiplier: int = 20
+    FIRST_RUN_RATIO: int = 18
+    SECOND_RUN_RATIO: int = 20
 
     def get_spent_calories(self) -> float:
         """Получить количество затраченных калорий."""
-        return ((self.frst_run_multiplier
+        return ((self.FIRST_RUN_RATIO
                  * self.get_mean_speed()
-                 - self.scnd_run_multiplier)
+                 - self.SECOND_RUN_RATIO)
                 * self.weight / self.M_IN_KM
-                * (self.duration * self.HOUR_IN_MIN))
+                * (self.duration * self.HOUR_TO_MIN))
 
 
 class SportsWalking(Training):
     """Тренировка: спортивная ходьба."""
-    frst_wlk_multiplier: float = 0.035
-    scnd_wlk_multiplier: float = 0.029
+    FIRST_WALK_RATIO: float = 0.035
+    SECOND_WALK_RATIO: float = 0.029
 
     def __init__(self,
                  action: int,
@@ -91,17 +86,17 @@ class SportsWalking(Training):
 
     def get_spent_calories(self) -> float:
         """Получить количество затраченных калорий."""
-        return (self.frst_wlk_multiplier * self.
-                weight + (self.get_mean_speed() ** 2 // self.
-                          height) * self.scnd_wlk_multiplier * self.
-                weight) * (self.duration * self.HOUR_IN_MIN)
+        return ((self.FIRST_WALK_RATIO * self.weight
+                 + (self.get_mean_speed() ** 2 // self.height)
+                 * self.SECOND_WALK_RATIO * self.weight)
+                * (self.duration * self.HOUR_TO_MIN))
 
 
 class Swimming(Training):
     """Тренировка: плавание."""
     LEN_STEP: float = 1.38  # константа для перевода движений в пловки =)
-    frst_swm_multiplier: float = 1.1
-    scnd_swm_multiplier: int = 2
+    FIRST_SWIM_RATIO: float = 1.1
+    SECOND_SWIM_RATIO: int = 2
 
     def __init__(self,
                  action: int,
@@ -123,19 +118,19 @@ class Swimming(Training):
     def get_spent_calories(self) -> float:
         """Получить количество затраченных калорий."""
         return ((self.get_mean_speed()
-                + self.frst_swm_multiplier)
-                * self.scnd_swm_multiplier
+                + self.FIRST_SWIM_RATIO)
+                * self.SECOND_SWIM_RATIO
                 * self.weight)
 
 
 def read_package(workout: str, workout_data: List[int]) -> Training:
     """Прочитать данные полученные от датчиков."""
-
-    sport_type = {'SWM': Swimming, 'RUN': Running, 'WLK': SportsWalking}
-    try:
+    sport_type: Dict[str, Type[Training]] = {'SWM': Swimming, 'RUN': Running,
+                                             'WLK': SportsWalking}
+    if workout in sport_type:
         return sport_type[workout](*workout_data)
-    except LookupError:
-        print(f"Исключение KeyError. Ключа '{workout}' нет в словаре.")
+    else:
+        raise KeyError
 
 
 def main(train: Training) -> None:
@@ -149,6 +144,7 @@ if __name__ == '__main__':
         ('SWM', [720, 1, 80, 25, 40]),
         ('RUN', [15000, 1, 75]),
         ('WLK', [9000, 1, 75, 180]),
+        ('ggg', [9000, 1, 75, 180]),
     ]
 
     for workout_type, data in packages:
